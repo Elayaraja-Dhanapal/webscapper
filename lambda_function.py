ed75f14c8b5674b17487776781643b8c2a80fa12
Elayaraja Dhanapal <elayaraja.dhanapal@gmail.com>
2022-01-28 23:57:04 +0530

    Add files via upload
    Scrape using webpage instead of rss

--- lambda_function.py
+++ lambda_function.py
@@ -1,58 +1,58 @@
-import botocore
-import boto3
 import requests
 from bs4 import BeautifulSoup
-import json
 
 
-AWS_REGION_RSS_FEEDS = {'us-east-1': ['https://status.aws.amazon.com/rss/ecr-us-east-1.rss',
-                                      'https://status.aws.amazon.com/rss/dynamodb-us-east-1.rss',
-                                      'https://status.aws.amazon.com/rss/apigateway-us-east-1.rss',
-                                      'https://status.aws.amazon.com/rss/eks-us-east-1.rss',
-                                      'https://status.aws.amazon.com/rss/route53-us-east-1.rss',
-                                      'https://status.aws.amazon.com/rss/codecommit-us-east-1.rss'], 'us-east-2': ['https://status.aws.amazon.com/rss/ecr-us-east-2.rss',
-                                                                                                                   'https://status.aws.amazon.com/rss/dynamodb-us-east-2.rss',
-                                                                                                                   'https://status.aws.amazon.com/rss/apigateway-us-east-2.rss',
-                                                                                                                   'https://status.aws.amazon.com/rss/eks-us-east-2.rss',
-                                                                                                                   'https://status.aws.amazon.com/rss/route53-us-east-2.rss',
-                                                                                                                   'https://status.aws.amazon.com/rss/codecommit-us-east-2.rss'], 'us-west-1': ['https://status.aws.amazon.com/rss/ecr-us-west-1.rss',
-                                                                                                                                                                                                'https://status.aws.amazon.com/rss/dynamodb-us-west-1.rss',
-                                                                                                                                                                                                'https://status.aws.amazon.com/rss/apigateway-us-west-1.rss',
-                                                                                                                                                                                                'https://status.aws.amazon.com/rss/eks-us-west-1.rss',
-                                                                                                                                                                                                'https://status.aws.amazon.com/rss/route53-us-west-1.rss',
-                                                                                                                                                                                                'https://status.aws.amazon.com/rss/codecommit-us-west-1.rss'], 'us-west-2': ['https://status.aws.amazon.com/rss/ecr-us-west-2.rss',
-                                                                                                                                                                                                                                                                             'https://status.aws.amazon.com/rss/dynamodb-us-west-2.rss',
-                                                                                                                                                                                                                                                                             'https://status.aws.amazon.com/rss/apigateway-us-west-2.rss',
-                                                                                                                                                                                                                                                                             'https://status.aws.amazon.com/rss/eks-us-west-2.rss',
-                                                                                                                                                                                                                                                                             'https://status.aws.amazon.com/rss/route53-us-west-2.rss',
-                                                                                                                                                                                                                                                                             'https://status.aws.amazon.com/rss/codecommit-us-west-2.rss']}
-
+AWS_HEALTH_STATUS_WEB_URL = 'https://status.aws.amazon.com/'
 
 def lambda_handler(event, context):
     article_list = []
-    for aws_region, feeds in AWS_REGION_RSS_FEEDS.items():
-        for url in feeds:
-            r = requests.get(url)        
-            soup = BeautifulSoup(r.content)
-            articles = soup.findAll('item')
-            aws_service = soup.find('title').text            
-            for a in articles:            
-                title = a.find('title').text
-                link = a.find('link').text                
-                published = a.find('pubdate').text
-                article = {
-                    'region': aws_region,
-                    'service': aws_service,
-                    'health': title,                
-                    'published': published
+    r = requests.get(AWS_HEALTH_STATUS_WEB_URL)        
+    soup = BeautifulSoup(r.content)    
+    div = soup.find('div', attrs= {'id':'NA_block'})    
+    tables = div.find_all('table')    
+    service_table = tables[1]
+    rows = service_table.tbody.find_all('tr')
+    for row in rows:
+        cells = row.find_all('td')
+        if 'Amazon DynamoDB' in cells[1].string:
+            article = {
+                'service': cells[1].string,
+                'health': cells[2].string
                 }
-                article_list.append(article)
-                break
-
-
+            article_list.append(article)
+        if 'Amazon ECR' in cells[1].string:
+            article = {
+                'service': cells[1].string,
+                'health': cells[2].string
+                }
+            article_list.append(article)
+        if 'Amazon API Gateway' in cells[1].string:
+            article = {
+                'service': cells[1].string,
+                'health': cells[2].string
+                }
+            article_list.append(article)
+        if 'Amazon Elastic Kubernetes' in cells[1].string:
+            article = {
+                'service': cells[1].string,
+                'health': cells[2].string
+                }
+            article_list.append(article)
+        if 'Amazon Route 53' in cells[1].string:
+            article = {
+                'service': cells[1].string,
+                'health': cells[2].string
+                }
+            article_list.append(article)
+        if 'AWS CodeCommit' in cells[1].string:
+            article = {
+                'service': cells[1].string,
+                'health': cells[2].string
+                }
+            article_list.append(article)
     return {
         'statusCode': 200,
-        'body': json.dumps(article_list)
+        'body': article_list
     }
 
 
